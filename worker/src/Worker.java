33cb9933b551b95a2b41e55ef12a2b28c1845c59
Viacheslav <bekirov303@yandex.ru>
2023-07-05 19:30:25 +0300

    firs commit

---
+++ worker/src/Worker.java
@@ -0,0 +1,29 @@
+public class Worker {
+    private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallback;
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+
+    }
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            if (i == 33) {
+                errorCallback.onError("Error" + i);
+            } else {
+                callback.onDone("Task " + i + " is done");
+            }
+        }
+    }
+
+    @FunctionalInterface
+    public interface OnTaskDoneListener {
+        void onDone(String result);
+    }
+    @FunctionalInterface
+    public interface OnTaskErrorListener {
+        void onError(String error);
+    }
+
+
+}
